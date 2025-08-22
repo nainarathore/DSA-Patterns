2e526df6ff8be1412deb73f38b10d565b829ebc4
Naina Rathore <142399518+nainarathore@users.noreply.github.com>
2025-08-22 22:24:28 +0530

    Implement diamond shape pattern in P7-Diamond shape.py

---
+++ P7-Diamond shape.py
@@ -0,0 +1,21 @@
+#include<bits/c++.h>
+using namespace std;
+void P5()
+void P6()
+int main(){
+# consecutively run the codes of pattern 5 and pattern 6
+   void P5();
+   void P6();
+}
+
+output:
+
+        *
+      * * *
+    * * * * *
+  * * * * * * *
+* * * * * * * * *
+  * * * * * * *
+    * * * * *
+      * * *
+        *

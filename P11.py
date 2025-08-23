ead0d7bf09db79afd1cfc87a6f40a7cb2dae1429
Naina Rathore <142399518+nainarathore@users.noreply.github.com>
2025-08-23 22:08:16 +0530

    Add pattern printing program in C++

---
+++ P11.py
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int space = 2*(n-1);
+    for(int i=1; i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<j;
+        }
+        for(int j=1;j<=space;j++)
+        {
+            cout<<" ";
+        }
+        for(int j=i;j>=1;j--)
+        {
+            cout<<j;
+        }
+        cout<<endl;
+        space -=2;
+    }
+}
+
+output:
+
+5
+
+1                 1
+1 2             2 1
+1 2 3         3 2 1
+1 2 3 4     4 3 2 1
+1 2 3 4 5 5 4 3 2 1
